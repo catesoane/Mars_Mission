c2f59abc8366fe4f5670ee9630700ed74b739171
Cate Soane <33916594+catesoane@users.noreply.github.com>
2018-11-14 15:49:41 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Twitter API Key / Google API
+
+consumer_key = "YDHkuUEXckelNAWCr4hB4TnWb"
+consumer_secret = "CD3sAWN0hmgTz4YmunR13Aghh92r8VimnHManEx7LaQ2v1ennY"
+access_token = "1446489535-2Uxb3oSjUhjYK6TAMTKIixK2vVabG6KWIcHe5Mb"
+access_token_secret = "NISAXxbxXdaPT2qixqrbP8eFz8pjGtvRRSfFZeES8FXVd"
+
+gkey = "AIzaSyDJLIZeQsZvl9JyfqB6u6FW6-U9n41RY1k"
